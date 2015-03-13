d82c50c1662c0f3717b3ab05a9cfa2d86a891ea0
Simon Baslé <simon.basle@gmail.com>
2015-03-13 15:59:27 +0100

    adding the User domain object

---
+++ src/main/java/org/dogepool/practicalrx/domain/User.java
@@ -0,0 +1,20 @@
+package org.dogepool.practicalrx.domain;
+
+public class User {
+
+    public static final User USER = new User(0L, "user0", "Test User", "Story of my life.\nEnd of Story.", "12434");
+
+    public final long id;
+    public final String nickname;
+    public final String displayName;
+    public final String bio;
+    public final String avatarId;
+
+    public User(long id, String nickname, String displayName, String bio, String avatarId) {
+        this.id = id;
+        this.nickname = nickname;
+        this.displayName = displayName;
+        this.bio = bio;
+        this.avatarId = avatarId;
+    }
+}
