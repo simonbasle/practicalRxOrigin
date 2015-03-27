9ae00c0c2f12c9ccc0318b6f31b3fefb57e7e010
Simon Baslé <simon.basle@gmail.com>
2015-03-27 17:46:51 +0100

    use Spring's rest template for REST api calls

--- src/main/java/org/dogepool/practicalrx/services/ExchangeRateService.java
+++ src/main/java/org/dogepool/practicalrx/services/ExchangeRateService.java
@@ -2,14 +2,10 @@ package org.dogepool.practicalrx.services;
 
 import java.util.Map;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import retrofit.client.Client;
-import retrofit.client.Request;
-import retrofit.client.Response;
-import retrofit.client.UrlConnectionClient;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * A facade service to get DOGE to USD and DOGE to other currencies exchange rates.
@@ -24,9 +20,7 @@ public class ExchangeRateService {
     private String exchangeUrl;
 
     @Autowired
-    private ObjectMapper mapper;
-
-    private Client restClient = new UrlConnectionClient();
+    private RestTemplate restTemplate;
 
     public Double dogeToCurrencyExchangeRate(String targetCurrencyCode) {
         //get the doge-dollar rate
@@ -40,41 +34,19 @@ public class ExchangeRateService {
     }
 
     private double dogeToDollar() {
-        //TODO timeouts?
-        try {
-            Response response = restClient.execute(new Request("GET", dogeUrl + "/", null, null));
-            if (response.getStatus() == 200) {
-                Double rate = mapper.readValue(response.getBody().in(), Double.class);
-                return rate.doubleValue();
-            } else {
-                throw new RuntimeException("Error getting doge to dollar: "
-                        + response.getStatus() + " - " + response.getReason());
-            }
-        } catch (Exception e) {
-            throw new IllegalStateException("Cannot get doge to dollar", e);
-        }
+        return restTemplate.getForObject(dogeUrl, Double.class);
     }
 
     private double dollarToCurrency(String currencyCode) {
-        //TODO timeouts, reading error body
-        try {
-            Response response = restClient.execute(new Request("GET", exchangeUrl + "/USD/" + currencyCode
-                    , null, null));
-            if (response.getStatus() == 200) {
-                Map<String, Object> result = mapper.readValue(response.getBody().in(), Map.class);
-                Double rate = (Double) result.get("exchangeRate");
-                if (rate == null)
-                    rate = (Double) result.get("rate");
-                if (rate == null) {
-                    throw new IllegalArgumentException("Malformed exchange rate");
-                }
-                return rate.doubleValue();
-            } else {
-                throw new IllegalArgumentException("Error getting USD to " + currencyCode + ": "
-                        + response.getStatus() + " - " + response.getReason());
-            }
-        } catch (Exception e) {
-            throw new IllegalStateException("Cannot get USD to " + currencyCode + ": " + e.getMessage(), e);
+        Map result = restTemplate.getForObject(exchangeUrl + "/{from}/{to}", Map.class,
+                "USD", currencyCode);
+        Double rate = (Double) result.get("exchangeRate");
+        if (rate == null)
+            rate = (Double) result.get("rate");
+
+        if (rate == null) {
+            throw new IllegalArgumentException("Malformed exchange rate");
         }
+        return rate;
     }
 }
