9d8e6a9ae347b8ad346d50d7558f562baee45bb3
Simon Baslé <simon.basle@gmail.com>
2015-03-27 17:47:14 +0100

    prepare a Couchbase Bucket reference to be injected

---
+++ src/main/java/org/dogepool/practicalrx/internal/config/CouchbaseStorageConfiguration.java
@@ -0,0 +1,26 @@
+package org.dogepool.practicalrx.internal.config;
+
+import com.couchbase.client.java.Bucket;
+import com.couchbase.client.java.Cluster;
+import com.couchbase.client.java.CouchbaseCluster;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CouchbaseStorageConfiguration {
+
+    @Bean(destroyMethod = "disconnect")
+    public Cluster couchbaseCluster( @Value("#{'${store.nodes:127.0.0.1}'.split(',')}")  String... nodes) {
+        return CouchbaseCluster.create(nodes);
+    }
+
+    @Bean
+    @Autowired
+    public Bucket couchbaseBucket( Cluster cluster,
+            @Value("${store.bucket:default}") String bucket,
+            @Value("${store.bucket.password:}") String password) {
+        return cluster.openBucket(bucket, password);
+    }
+}
