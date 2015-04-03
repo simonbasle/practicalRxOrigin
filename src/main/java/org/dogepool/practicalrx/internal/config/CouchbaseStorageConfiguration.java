7de2fbe950e36327b248aa65290327d698e34a20
Simon Baslé <simon.basle@gmail.com>
2015-04-03 23:21:19 +0200

    add store to configuration and allow disabling it for findAll

--- src/main/java/org/dogepool/practicalrx/internal/config/CouchbaseStorageConfiguration.java
+++ src/main/java/org/dogepool/practicalrx/internal/config/CouchbaseStorageConfiguration.java
@@ -3,6 +3,8 @@ package org.dogepool.practicalrx.internal.config;
 import com.couchbase.client.java.Bucket;
 import com.couchbase.client.java.Cluster;
 import com.couchbase.client.java.CouchbaseCluster;
+import com.couchbase.client.java.env.CouchbaseEnvironment;
+import com.couchbase.client.java.env.DefaultCouchbaseEnvironment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -11,10 +13,15 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class CouchbaseStorageConfiguration {
 
+    @Value("${store.enableFindAll:false}")
+    private boolean useCouchbaseForFindAll;
+
     @Bean(destroyMethod = "disconnect")
     public Cluster couchbaseCluster( @Value("#{'${store.nodes:127.0.0.1}'.split(',')}")  String... nodes) {
-        System.setProperty("com.couchbase.queryEnabled", "true");
-        return CouchbaseCluster.create(nodes);
+        CouchbaseEnvironment env = DefaultCouchbaseEnvironment.builder()
+                .queryEnabled(useCouchbaseForFindAll)
+                .build();
+        return CouchbaseCluster.create(env, nodes);
     }
 
     @Bean
