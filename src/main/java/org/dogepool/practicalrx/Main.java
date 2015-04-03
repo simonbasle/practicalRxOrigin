ee6f842d752efcf49697e0d7c3c3c63d33590616
Laurent Doguin <laurent.doguin@couchbase.com>
2015-04-03 14:39:07 +0200

    use commandLine Runner and injection

--- src/main/java/org/dogepool/practicalrx/Main.java
+++ src/main/java/org/dogepool/practicalrx/Main.java
@@ -7,44 +7,48 @@ import org.dogepool.practicalrx.domain.UserStat;
 import org.dogepool.practicalrx.services.ExchangeRateService;
 import org.dogepool.practicalrx.services.PoolService;
 import org.dogepool.practicalrx.services.RankingService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
-        ApplicationContext ctx = SpringApplication.run(Main.class, args);
+        ConfigurableApplicationContext ctx = SpringApplication.run(Main.class, args);
+        ctx.close();
+    }
 
-        RankingService rankinService = ctx.getBean(RankingService.class);
-        PoolService poolService = ctx.getBean(PoolService.class);
-        ExchangeRateService exchangeRateService = ctx.getBean(ExchangeRateService.class);
+    @Bean
+    CommandLineRunner commandLineRunner(RankingService rankinService, PoolService poolService, ExchangeRateService exchangeRateService) {
+        return args -> {
+            //connect USER automatically
+            poolService.connectUser(User.USER);
 
-        //connect USER automatically
-        poolService.connectUser(User.USER);
+            //display welcome screen in console
+            List<UserStat> hashLadder = rankinService.getLadderByHashrate();
+            List<UserStat> coinsLadder = rankinService.getLadderByCoins();
 
-        //display welcome screen in console
-        List<UserStat> hashLadder = rankinService.getLadderByHashrate();
-        List<UserStat> coinsLadder = rankinService.getLadderByCoins();
+            System.out.println("Welcome to " + poolService.poolName() + " dogecoin mining pool!");
+            System.out.println(poolService.miningUsers().size() + " users currently mining, for a global hashrate of "
+                    + poolService.poolGigaHashrate() + " GHash/s");
 
-        System.out.println("Welcome to " + poolService.poolName() + " dogecoin mining pool!");
-        System.out.println(poolService.miningUsers().size() + " users currently mining, for a global hashrate of "
-            + poolService.poolGigaHashrate() + " GHash/s");
+            System.out.println("1 DOGE = " + exchangeRateService.dogeToCurrencyExchangeRate("USD") + "$");
+            System.out.println("1 DOGE = " + exchangeRateService.dogeToCurrencyExchangeRate("EUR") + "€");
 
-        System.out.println("1 DOGE = " + exchangeRateService.dogeToCurrencyExchangeRate("USD") + "$");
-        System.out.println("1 DOGE = " + exchangeRateService.dogeToCurrencyExchangeRate("EUR") + "€");
+            System.out.println("\n----- TOP 10 Miners by Hashrate -----");
+            int count = 1;
+            for (UserStat userStat : hashLadder) {
+                System.out.println(count++ + ": " + userStat.user.nickname + ", " + userStat.hashrate + " GHash/s");
+            }
 
-        System.out.println("\n----- TOP 10 Miners by Hashrate -----");
-        int count = 1;
-        for (UserStat userStat : hashLadder) {
-            System.out.println(count++ + ": " + userStat.user.nickname + ", " + userStat.hashrate + " GHash/s");
-        }
-
-        System.out.println("\n----- TOP 10 Miners by Coins Found -----");
-        count = 1;
-        for (UserStat userStat : coinsLadder) {
-            System.out.println(count++ + ": " + userStat.user.nickname + ", " + userStat.totalCoinsMined + " dogecoins");
-        }
+            System.out.println("\n----- TOP 10 Miners by Coins Found -----");
+            count = 1;
+            for (UserStat userStat : coinsLadder) {
+                System.out.println(count++ + ": " + userStat.user.nickname + ", " + userStat.totalCoinsMined + " dogecoins");
+            }
+        };
     }
 }
