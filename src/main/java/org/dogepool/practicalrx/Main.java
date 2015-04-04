533f708cc06cdd440fb447790f752561796088a0
Simon Baslé <simon.basle@gmail.com>
2015-04-04 16:20:57 +0200

    don't close the context right away

--- src/main/java/org/dogepool/practicalrx/Main.java
+++ src/main/java/org/dogepool/practicalrx/Main.java
@@ -21,7 +21,6 @@ public class Main {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext ctx = SpringApplication.run(Main.class, args);
-        ctx.close();
     }
 
     @Bean
