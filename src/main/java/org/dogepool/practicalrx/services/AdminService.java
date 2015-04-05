2ed767766bdaac6a7caf96198c47504b1200a04d
Simon Baslé <simon.basle@gmail.com>
2015-04-05 18:48:41 +0200

    exchange now falls back on costly service + track cost

--- src/main/java/org/dogepool/practicalrx/services/AdminService.java
+++ src/main/java/org/dogepool/practicalrx/services/AdminService.java
@@ -3,6 +3,7 @@ package org.dogepool.practicalrx.services;
 import java.math.BigInteger;
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.concurrent.atomic.LongAdder;
 
 import org.springframework.stereotype.Service;
 import rx.Observable;
@@ -13,6 +14,12 @@ import rx.Observable;
 @Service
 public class AdminService {
 
+    private LongAdder currentCosts = new LongAdder();
+
+    public void addCost(int cost) {
+        this.currentCosts.add(cost);
+    }
+
     public Observable<BigInteger> costForMonth(int year, Month month) {
         return Observable
                 .<LocalDate>create(s -> {
@@ -20,6 +27,9 @@ public class AdminService {
                     s.onCompleted();
                 })
                 .map(now -> {
+                    if (year == now.getYear() && month == now.getMonth()) {
+                        return BigInteger.valueOf(currentCosts.longValue());
+                    }
                     if (year > now.getYear()
                             || year == now.getYear() && month.getValue() > now.getMonthValue()) {
                         return BigInteger.ZERO;
--- src/main/java/org/dogepool/practicalrx/services/ExchangeRateService.java
+++ src/main/java/org/dogepool/practicalrx/services/ExchangeRateService.java
@@ -1,12 +1,15 @@
 package org.dogepool.practicalrx.services;
 
 import java.util.Map;
+import java.util.concurrent.TimeoutException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
 import rx.Observable;
+import rx.functions.Func1;
 
 /**
  * A facade service to get DOGE to USD and DOGE to other currencies exchange rates.
@@ -20,13 +23,24 @@ public class ExchangeRateService {
     @Value("${exchange.free.api.baseUrl}")
     private String exchangeUrl;
 
+    @Value("${exchange.nonfree.api.baseUrl}")
+    private String exchangeNonfreeUrl;
+
+    @Autowired
+    private AdminService adminService;
+
     @Autowired
     private RestTemplate restTemplate;
 
     public Observable<Double> dogeToCurrencyExchangeRate(String targetCurrencyCode) {
+        Observable<Double> dollarToCurrency =
+                dollarToCurrency(targetCurrencyCode)
+                .onErrorResumeNext(t -> (t instanceof ResourceAccessException)
+                        ? dollarToCurrencyNonFree(targetCurrencyCode)
+                        : Observable.error(t));
+
         return dogeToDollar()
-                .zipWith(dollarToCurrency(targetCurrencyCode),
-                        (doge2usd, usd2currency) -> doge2usd * usd2currency);
+                .zipWith(dollarToCurrency, (doge2usd, usd2currency) -> doge2usd * usd2currency);
     }
 
     private Observable<Double> dogeToDollar() {
@@ -55,5 +69,23 @@ public class ExchangeRateService {
             sub.onNext(rate);
             sub.onCompleted();
         });
+    }
+
+    private Observable<Double> dollarToCurrencyNonFree(String currencyCode) {
+        return Observable.<Double>create(sub -> {
+            Map result = restTemplate.getForObject(exchangeNonfreeUrl + "/{from}/{to}", Map.class,
+                    "USD", currencyCode);
+            Double rate = (Double) result.get("exchangeRate");
+            if (rate == null)
+                rate = (Double) result.get("rate");
+
+            if (rate == null) {
+                sub.onError(new IllegalArgumentException("Malformed exchange rate"));
+            }
+            sub.onNext(rate);
+            sub.onCompleted();
+        })
+        .doOnNext(r -> adminService.addCost(1))
+        .doOnNext(r -> System.out.println("CALLED NON-FREE EXCHANGE RATE SERVICE FOR 1$"));
     }
 }
