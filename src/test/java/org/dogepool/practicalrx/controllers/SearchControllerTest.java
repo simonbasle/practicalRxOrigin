a6700f33332e92344654a03284b2b0ca598d814c
Simon Baslé <simon.basle@gmail.com>
2015-04-16 00:47:27 +0200

    added more controller unit tests

--- src/test/java/org/dogepool/practicalrx/controllers/SearchControllerTest.java
+++ src/test/java/org/dogepool/practicalrx/controllers/SearchControllerTest.java
@@ -1,6 +1,14 @@
 package org.dogepool.practicalrx.controllers;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.List;
+
 import org.dogepool.practicalrx.Main;
+import org.dogepool.practicalrx.domain.User;
+import org.dogepool.practicalrx.domain.UserStat;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,6 +18,8 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -29,11 +39,61 @@ public class SearchControllerTest {
 
     @Test
     public void testSearchByName() throws Exception {
-        //TODO
+        String expected = "[{\"id\":1,\"nickname\":\"richUser\",\"displayName\":\"Richie Rich\"," +
+                "\"bio\":\"I'm rich I have dogecoin\",\"avatarId\":\"45678\",\"type\":\"user\"}]";
+
+        mockMvc.perform(get("/search/user/{pattern}", "richie"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(expected));
     }
 
     @Test
-    public void testSearchByCoins() throws Exception {
-        //TODO
+    public void testSearchByNameNoResult() throws Exception {
+        mockMvc.perform(get("/search/user/{pattern}", "alfred"))
+                .andExpect(status().isOk())
+                .andExpect(content().json("[]"));
+    }
+
+    @Test
+    public void testSearchByCoinsMinOnly() throws Exception {
+        String expected = "[{\"user\":{\"id\":1,\"nickname\":\"richUser\",\"displayName\":\"Richie Rich\"," +
+                "\"bio\":\"I'm rich I have dogecoin\",\"avatarId\":\"45678\",\"type\":\"user\"}," +
+                "\"hashrate\":-1.0,\"totalCoinsMined\":12}]";
+
+        mockMvc.perform(get("/search/user/coins/{min}", 10))
+               .andExpect(status().isOk())
+               .andExpect(content().json(expected));
+    }
+
+    @Test
+    public void testSearchByCoinsMinNoMatch() throws Exception {
+        mockMvc.perform(get("/search/user/coins/{min}", 1200))
+               .andExpect(status().isOk())
+               .andExpect(content().json("[]"));
+    }
+
+    @Test
+    public void testSearchByCoinsMinMax() throws Exception {
+        String expected = "[{\"user\":{\"id\":1,\"nickname\":\"richUser\",\"displayName\":\"Richie Rich\"," +
+                "\"bio\":\"I'm rich I have dogecoin\",\"avatarId\":\"45678\",\"type\":\"user\"}," +
+                "\"hashrate\":-1.0,\"totalCoinsMined\":12}]";
+
+        mockMvc.perform(get("/search/user/coins/{min}/{max}", 10, 13))
+               .andExpect(status().isOk())
+               .andExpect(content().json(expected));
+    }
+
+    @Test
+    public void testSearchByCoinsMinMaxNoMinMatch() throws Exception {
+        mockMvc.perform(get("/search/user/coins/{min}/{max}", 13, 14))
+               .andExpect(status().isOk())
+               .andExpect(content().json("[]"));
+    }
+
+    @Test
+    public void testSearchByCoinsMinMaxNoMaxMatch() throws Exception {
+        mockMvc.perform(get("/search/user/coins/{min}/{max}", 1, 11))
+               .andExpect(status().isOk())
+               .andExpect(content().json("[]"));
     }
 }
--- src/test/java/org/dogepool/practicalrx/controllers/UserProfileControllerTest.java
+++ src/test/java/org/dogepool/practicalrx/controllers/UserProfileControllerTest.java
@@ -1,11 +1,24 @@
 package org.dogepool.practicalrx.controllers;
 
+import static org.hamcrest.beans.HasPropertyWithValue.hasProperty;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.request;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.dogepool.practicalrx.Main;
+import org.dogepool.practicalrx.error.*;
+import org.dogepool.practicalrx.error.Error;
+import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -29,6 +42,22 @@ public class UserProfileControllerTest {
 
     @Test
     public void testProfile() throws Exception {
-        //TODO
+        String expected = "";
+
+        mockMvc.perform(get("/miner/{id}", 1).accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(expected));
+    }
+
+    @Test
+    public void testProfileNotFound() throws Exception {
+        DogePoolException expected = new DogePoolException("Unknown miner",
+                org.dogepool.practicalrx.error.Error.UNKNOWN_USER,
+                HttpStatus.NOT_FOUND);
+
+        mockMvc.perform(get("/miner/{id}", 1000).accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(request().asyncStarted())
+                .andExpect(request().asyncResult(expected));
     }
 }
