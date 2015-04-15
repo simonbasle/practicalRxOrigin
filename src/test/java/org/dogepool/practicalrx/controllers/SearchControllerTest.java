78f0678628f2ea1cc53724dded8f93aa001f3492
Simon Baslé <simon.basle@gmail.com>
2015-04-15 18:14:37 +0200

    added unit tests to check most controllers

---
+++ src/test/java/org/dogepool/practicalrx/controllers/SearchControllerTest.java
@@ -0,0 +1,39 @@
+package org.dogepool.practicalrx.controllers;
+
+import org.dogepool.practicalrx.Main;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Main.class)
+@WebAppConfiguration
+public class SearchControllerTest {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() throws Exception {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void testSearchByName() throws Exception {
+        //TODO
+    }
+
+    @Test
+    public void testSearchByCoins() throws Exception {
+        //TODO
+    }
+}
